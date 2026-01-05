ab207e66a8d5a82cd8f408e1432b407892359f0c
Shanmuga Priya <shanmugapriya.k@ooranilabs.com>
2026-01-05 22:03:37 +0530

    updation

--- envoy.blade.php
+++
@@ -1,8 +0,0 @@
-@servers(['web' => 'digitalocean'])
-
-@task('deploy', ['on' => 'web'])
-    cd /var/www/myapp
-    git pull origin main
-    php artisan migrate --force
-    php artisan optimize:clear
-@endtask
