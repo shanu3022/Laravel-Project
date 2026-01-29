f9b6035f7efd89aa14f7080fc81f89d5d46cb2eb
root <kshanmugapriya223@gmail.com>
2026-01-29 17:09:47 +0000

    larabug installation

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/larabug-test', function () {
+    throw new Exception('Larabug is working on DigitalOcean');
+});
